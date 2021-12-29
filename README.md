d97f2a00069b8ab29654a2721896d7ea1a9538cf
desmond <minhngoc02hn@gmail.com>
2021-12-29 08:38:20 +0700

    features added

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Codeforces Test Parser for C/C++ on Windows
+# Codeforces Test Parser for C/C++ & Python on Windows
 
 <p align="center">
   <img src="https://i.ibb.co/82yzz2Q/example.png" />
 </p>
 
 ### Installation
-* Run ```pip install beautifulsoup4 requests psutil``` in your terminal.
+* Run ```pip install beautifulsoup4 requests``` in your terminal.
 * Clone this repository!
 * Change ```workspace_folder``` in ```run.py``` to the folder where your source files are located.
 * Please make some changes in ```run.py``` to make it suitable for your folder structure. Below is mine.
@@ -38,5 +38,7 @@ For example: ```.\parse 1620 A B C D E``` to download problem A, B, C, D, and E 
 For example: ```.\run A``` for testing problem A.
 
 * In case your default application for ```.py``` files is not Python, e.g. PyCharm, VSCode, etc. Please use ```python parse.py``` instead of ```.\parse``` and ```python run.py``` instead of ```.\run```.
+
+* This parser is C++-first, which means it will build Python file only after no C++ file has been found.
 
 If you find this repository useful, please give it a star :star:!
--- run.py
+++ run.py
@@ -6,7 +6,6 @@ import os
 import sys
 import subprocess
 import time
-import psutil
 from threading import Timer
 
 # workspace folder
@@ -18,6 +17,91 @@ contest_id = os.listdir(base_folder)[0]
 TIMEOUT_SECOND = 5
 
 
+def run_sample_py(problem_id):
+    # these are paths to py file
+    # please change this to suit your folder structure
+    # in this case, filename is "E:\...\Codeforces\1620A.py"
+    # if your file is at "...\Codeforces\Educational Codeforces Round 119\A.py" for example
+    # then change path_to_cpp_file = f'{workspace_folder}\{contest_name}\{problem_id}.py'
+    # with contest_name = "Education Codeforces Round 119" and problem_id = "A"
+
+    print(f'Building ...{workspace_folder.split()[-1]}\{contest_id}{problem_id}.py...')
+    path_to_py_file = f'{workspace_folder}\{contest_id}{problem_id}.py'
+    print('Successfully built!')
+    print('-' * 10)
+
+    n_cases = len(os.listdir(f'{base_folder}/{contest_id}/{problem_id}')) // 2  # number of sample test cases
+    n_correct = 0  # count correct test cases
+    TLE = False
+
+    for i in range(n_cases):
+        if TLE:
+            continue
+        print(f'Sample case #{i + 1}:', end=' ')
+        with open(f'{base_folder}/{contest_id}/{problem_id}/input{i}.txt') as f:
+            inp = f.read()
+
+            start_time = time.time()
+            process = subprocess.Popen(f'python "{path_to_py_file}"', stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+            process.stdin.write(str(inp).encode())
+
+            timer = Timer(TIMEOUT_SECOND + 0.001, process.kill)
+            try:
+                timer.start()
+                process.communicate()
+                end_time = time.time()
+                if end_time - start_time > TIMEOUT_SECOND:
+                    print('Time Limit Exceeded!')
+                    TLE = True
+                    continue
+            finally:
+                timer.cancel()
+
+            # pre-processing your output, do not change this
+            your_output = process.communicate()[0]
+            your_output = your_output.decode().strip()
+            your_output_original = your_output.rstrip('\n')
+            your_output = your_output.lower()
+            your_output = " ".join(your_output.splitlines())
+            your_output = " ".join(your_output.split()).rstrip('\n')
+
+            with open(f'{base_folder}/{contest_id}/{problem_id}/output{i}.txt') as f2:
+                # pre-processing actual output, do not change this
+                output = f2.read().strip()
+                output_original = output
+                output = output.lower()
+                output = " ".join(output.splitlines())
+                output = " ".join(output.split())
+
+                # compare your output vs actual output
+                if your_output == output:
+                    print('Correct!')
+                    n_correct += 1
+                else:
+                    print('Failed!')
+                print('-' * 10)
+
+                # input
+                # print('Input:')
+                # print(inp.rstrip('\n'))
+                # print('-' * 10)
+
+                # your output
+                print('Output:')
+                print(your_output_original)
+                # print(your_output) # debug
+                print('-' * 10)
+
+                # expected output
+                print('Expected:')
+                print(output_original)
+                # print(output) # debug
+                print('-' * 10)
+
+    print(f'Result: {n_correct} / {n_cases} test(s) passed!')
+
+
 def run_sample(problem_id):
     if not os.path.exists(f'{base_folder}\{contest_id}\{problem_id}'):
         print(f'You have not downloaded sample tests for problem {problem_id}!')
@@ -26,7 +110,10 @@ def run_sample(problem_id):
     print(f'Running problem {problem_id}...')
 
     if not os.path.exists(f'{workspace_folder}\{contest_id}{problem_id}.cpp'):
-        print(f'There is no source file for problem {problem_id}!')
+        if os.path.exists(f'{workspace_folder}\{contest_id}{problem_id}.py'):
+            run_sample_py(problem_id)
+        else:
+            print(f'There is no source file for problem {problem_id}!')
         return
 
     # these are paths to cpp file and exe file
@@ -37,14 +124,11 @@ def run_sample(problem_id):
     # with contest_name = "Education Codeforces Round 119" and problem_id = "A"
     # do not forget to change path_to_exe_file as well
 
+    print(f'Compiling ...{workspace_folder.split()[-1]}\{contest_id}{problem_id}.cpp...')
     path_to_cpp_file = f'{workspace_folder}\{contest_id}{problem_id}.cpp'
     path_to_exe_file = f'{workspace_folder}\{contest_id}{problem_id}.exe'
-    start_time = time.time()
     os.system(f'g++ "{path_to_cpp_file}" -o "{path_to_exe_file}')
-    end_time = time.time()
     print('Successfully compiled!')
-    print(f'Time: {round(end_time - start_time, 3)}(s)')
-    print(f'Memory: {psutil.Process(os.getpid()).memory_info().rss // 1024}(KB)')
     print('-' * 10)
 
     n_cases = len(os.listdir(f'{base_folder}/{contest_id}/{problem_id}')) // 2  # number of sample test cases
@@ -63,7 +147,7 @@ def run_sample(problem_id):
                                        stderr=subprocess.PIPE)
             process.stdin.write(str(inp).encode())
 
-            timer = Timer(TIMEOUT_SECOND + 0.1, process.kill)
+            timer = Timer(TIMEOUT_SECOND + 0.001, process.kill)
             try:
                 timer.start()
                 process.communicate()
@@ -79,16 +163,15 @@ def run_sample(problem_id):
             your_output = process.communicate()[0]
             your_output = your_output.decode().strip()
             your_output = your_output.lower()
-            original_your_output = your_output.rstrip('\n')
+            your_output_original = your_output.rstrip('\n')
             your_output = " ".join(your_output.splitlines())
             your_output = " ".join(your_output.split()).rstrip('\n')
-
 
             with open(f'{base_folder}/{contest_id}/{problem_id}/output{i}.txt') as f2:
                 # pre-processing actual output, do not change this
                 output = f2.read().strip()
                 output = output.lower()
-                original_output = output
+                output_original = output
                 output = " ".join(output.splitlines())
                 output = " ".join(output.split())
 
@@ -99,18 +182,21 @@ def run_sample(problem_id):
                 else:
                     print('Failed!')
                 print('-' * 10)
+
                 # input
-                print('Input:')
-                print(inp.rstrip('\n'))
-                print('-' * 10)
+                # print('Input:')
+                # print(inp.rstrip('\n'))
+                # print('-' * 10)
+
                 # your output
                 print('Output:')
-                print(original_your_output)
+                print(your_output_original)
                 # print(your_output) # debug
                 print('-' * 10)
+
                 # expected output
                 print('Expected:')
-                print(original_output)
+                print(output_original)
                 # print(output) # debug
                 print('-' * 10)
 
